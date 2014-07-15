325885962c589ebd9d375a9a8416594a35e92ab1
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2014-07-15 13:57:22 -0700

    Clean up Codec code

--- src/main/scala/com/persist/JsonFormat.scala
+++ src/main/scala/com/persist/JsonFormat.scala
@@ -1,76 +1,95 @@
 package com.persist
 
 import JsonOps._
+import scala.annotation.implicitNotFound
 
-package object Format {
+package object json {
 
-  def toJson(x: Any): Json = {
-    x match {
-      case _: Short | _: Int | _: Long | _: String | _: Boolean | _: Double | _: BigDecimal => x
-      case x: Map[_, _] => x.map { case (key, value) => (toJson(key), toJson(value))}
-      case x: Iterable[_] => x.map(toJson).toSeq
-      case Some(v) => toJson(v)
-      case None => jnull
-      case obj => obj.asInstanceOf[ {def toJson(): Json}].toJson()
+  trait ReadWriteCodec[T] extends ReadCodec[T] with WriteCodec[T]
+
+  object ReadWriteCodec {
+    trait SimpleCodec[T] extends ReadWriteCodec[T] {
+      def write(x: T):Json = x
+      def read(x: Json):T = x.asInstanceOf[T]
     }
   }
 
-  trait Readable[+A] {
-    def read(json: Json): A
+  @implicitNotFound(msg = "Cannot find ReadCodec for ${T}")
+  trait ReadCodec[+T] {
+    def read(json: Json): T
   }
 
-  object read {
-    def apply[A: Readable](json: Json): A = implicitly[Readable[A]].read(json)
-  }
-
-  object Readable {
-
-    implicit object ReadString extends Readable[String] {
-      def read(json: Json) = json.asInstanceOf[String]
+  object ReadCodec {
+    trait SimpleCodec[T] extends ReadCodec[T] {
+      def read(x: Json):T = x.asInstanceOf[T]
     }
+    implicit object StringCodec extends SimpleCodec[String]
+    implicit object IntCodec extends SimpleCodec[Int]
+    implicit object BooleanCodec extends SimpleCodec[Boolean]
+    implicit object LongCodec extends SimpleCodec[Long]
+    implicit object ShortCodec extends SimpleCodec[Short]
+    implicit object DoubleCodec extends SimpleCodec[Double]
+    implicit object BigDecimalCodec extends SimpleCodec[BigDecimal]
 
-    implicit object ReadShort extends Readable[Short] {
-      def read(json: Json) = json.asInstanceOf[Short]
-    }
+    def extractSeq[T: ReadCodec](json: Json): Seq[T] = json.asInstanceOf[JsonArray].map(implicitly[ReadCodec[T]].read(_))
 
-    implicit object ReadInt extends Readable[Int] {
-      def read(json: Json) = json.asInstanceOf[Int]
-    }
-
-    implicit object ReadLong extends Readable[Long] {
-      def read(json: Json) = json.asInstanceOf[Long]
-    }
-
-    implicit object ReadBoolean extends Readable[Boolean] {
-      def read(json: Json) = json.asInstanceOf[Boolean]
-    }
-
-    implicit object ReadDouble extends Readable[Double] {
-      def read(json: Json) = json.asInstanceOf[Double]
-    }
-
-    implicit object ReadBigDecimal extends Readable[BigDecimal] {
-      def read(json: Json) = json.asInstanceOf[BigDecimal]
-    }
-
-    def extractSeq[T: Readable](json: Json): Seq[T] = json.asInstanceOf[Seq[Json]].map(implicitly[Readable[T]].read(_))
-
-    implicit def readSet[T: Readable]: Readable[Set[T]] = new Readable[Set[T]] {
+    implicit def set[T: ReadCodec] = new ReadCodec[Set[T]] {
       def read(json: Json): Set[T] = extractSeq[T](json).toSet
     }
 
-    implicit def readList[T: Readable]: Readable[List[T]] = new Readable[List[T]] {
+    implicit def list[T: ReadCodec] = new ReadCodec[List[T]] {
       def read(json: Json): List[T] = extractSeq[T](json).toList
     }
 
-    implicit def readMap[T: Readable, X: Readable]: Readable[Map[T, X]] = new Readable[Map[T, X]] {
-      def read(json: Json): Map[T, X] = json.asInstanceOf[Map[Json, Json]].map { case (key, value) => (implicitly[Readable[T]].read(key), implicitly[Readable[X]].read(value))}
+    implicit def simpleMap[V: ReadCodec] = new ReadCodec[Map[String, V]] {
+      def read(json: Json): Map[String, V] = json.asInstanceOf[JsonObject].mapValues(implicitly[ReadCodec[V]].read(_))
     }
 
-    implicit def readOption[T: Readable]: Readable[Option[T]] = new Readable[Option[T]] {
-      def read(json: Json): Option[T] = if (json == jnull) None else Some(implicitly[Readable[T]].read(json))
+    implicit def complexMap[K: ReadCodec, V: ReadCodec] = new ReadCodec[Map[K, V]] {
+      def read(json: Json): Map[K, V] = json.asInstanceOf[JsonObject].map { case (key, value) =>
+        (implicitly[ReadCodec[K]].read(Json(key)), implicitly[ReadCodec[V]].read(value))
+      }
+    }
+
+    implicit def option[T: ReadCodec] = new ReadCodec[Option[T]] {
+      def read(json: Json): Option[T] = if (json == jnull) None else Some(implicitly[ReadCodec[T]].read(json))
     }
   }
+
+  @implicitNotFound(msg = "Cannot find WriteCodec for ${T}")
+  trait WriteCodec[-T] {
+    def write(obj: T): Json
+  }
+
+  object WriteCodec {
+    trait SimpleCodec[T] extends WriteCodec[T] {
+      def write(x: T):Json = x
+    }
+    implicit object StringCodec extends SimpleCodec[String]
+    implicit object IntCodec extends SimpleCodec[Int]
+    implicit object BooleanCodec extends SimpleCodec[Boolean]
+    implicit object LongCodec extends SimpleCodec[Long]
+    implicit object ShortCodec extends SimpleCodec[Short]
+    implicit object DoubleCodec extends SimpleCodec[Double]
+    implicit object BigDecimalCodec extends SimpleCodec[BigDecimal]
+    implicit def simpleMap[V: WriteCodec] = new WriteCodec[scala.collection.Map[String, V]] {
+      def write(obj: scala.collection.Map[String, V]): JsonObject = obj.mapValues(toJson(_)).toMap
+    }
+    implicit def complexMap[K: WriteCodec, V: WriteCodec] = new WriteCodec[scala.collection.Map[K,V]] {
+      def write(obj: scala.collection.Map[K, V]) = obj.map { case (key, value) =>
+        (Compact(implicitly[WriteCodec[K]].write(key)), implicitly[WriteCodec[V]].write(value))
+      }
+    }
+    implicit def iterable[V: WriteCodec] = new WriteCodec[Iterable[V]] {
+      def write(obj: Iterable[V]): JsonArray = obj.map(toJson(_)).toSeq
+    }
+    implicit def option[V: WriteCodec] = new WriteCodec[Option[V]] {
+      def write(obj: Option[V]): Json = obj.map(toJson(_)).getOrElse(jnull)
+    }
+  }
+
+  def toJson[T](obj: T)(implicit ev: WriteCodec[T]): Json = ev.write(obj)
+  def read[T](json: Json)(implicit ev: ReadCodec[T]): T = ev.read(json)
 }
 
 
